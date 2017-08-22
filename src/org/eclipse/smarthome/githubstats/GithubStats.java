61f2ab541ca67a50ab6a033fb7ac9955ebdcfaab
Stefan Triller <stefan.triller@telekom.de>
2017-08-22 11:19:50 +0200

    runnable jar file with parameters on command line

--- src/org/eclipse/smarthome/githubstats/GithubStats.java
+++ src/org/eclipse/smarthome/githubstats/GithubStats.java
@@ -12,39 +12,60 @@ public class GithubStats {
 
 	private static final String REPO_USER = "eclipse";
 	private static final String REPO_NAME = "smarthome";
-	public static final String OAUTH_TOKEN = "INSERT_YOUR_TOKEN_HERE";
-	
-//	private static final String SPAN_START = "2014-01-13";
-//	private static final String SPAN_END = "2017-06-28";
-	
-	private static final String SPAN_END = "2017-08-16";
-	
-	private static final int INTERVAL_COUNT = 26;
-	private static final int SPRINT_WEEKS = 2;	
 		
-	private static final int INTERVAL_DAYS = 14;
+	private static String SPAN_END;	
+	private static int NUM_INTERVALS;
+	private static int SPRINT_WEEKS;		
 	//directory containing the repo and the stats.sh file!
-	private static String GIT_WORKING_DIR = "/home/stefan/smarthome-master/git/smarthome";
+	private static String GIT_WORKING_DIR;
+	public static String OAUTH_TOKEN;
+
+	private static int INTERVAL_DAYS;
 	
 	public static void main(String[] args) throws IOException, ParseException {
+		
+		if(args.length == 5) {
+			SPAN_END = args[0];
+			NUM_INTERVALS = Integer.parseInt(args[1]);
+			SPRINT_WEEKS = Integer.parseInt(args[2]);
+			GIT_WORKING_DIR = args[3];
+			OAUTH_TOKEN = args[4];
+		} else if(args.length == 4) {
+			SPAN_END = args[0];
+			NUM_INTERVALS = Integer.parseInt(args[1]);
+			SPRINT_WEEKS = Integer.parseInt(args[2]);
+			GIT_WORKING_DIR = args[3];
+			OAUTH_TOKEN = null;
+		}
+		else {
+			System.out.println("Usage: java- jar GithubStats.jar <reviewDay(yyyy-mm-dd)> <numIntervals> <sprintWeeks> <path to stats.sh in repository> (<GitHubOAuthToken>)" );
+			System.out.println("Example: java- jar GithubStats.jar 2017-08-23 26 2 /home/USER/smarthome-master/git/smarthome xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" );
+		}
+		
+		INTERVAL_DAYS = SPRINT_WEEKS * 7;
+		
 		//org.kohsuke api		
-		GitHub github = GitHub.connect("eclipse", OAUTH_TOKEN);
-//		GitHub github = GitHub.connectAnonymously();
+		GitHub github;
+		if(OAUTH_TOKEN == null) {
+			github = GitHub.connectAnonymously();			
+		} else {
+			github = GitHub.connect("eclipse", OAUTH_TOKEN);
+		}
 		
 		GHRepository repo = github.getUser(REPO_USER).getRepository(REPO_NAME);
 
 		Date spanEnd = DateUtils.parseDate(SPAN_END, new String[]{"yyyy-MM-dd"});
+		//add a couple of seconds so github recognizes it as the next day
 		spanEnd = DateUtils.addSeconds(spanEnd, 15);
 		
-		int daysDiff = INTERVAL_COUNT * SPRINT_WEEKS * 7;
+		int daysDiff = NUM_INTERVALS * SPRINT_WEEKS * 7;
 		
 		Date spanStart = DateUtils.addDays(spanEnd, -daysDiff);	
 		Date tmpEnd = DateUtils.addDays(spanStart, INTERVAL_DAYS-1);
 
 		PersistStats persister = new PersistStats("smarthomeStats.csv");
 		while(tmpEnd.before(spanEnd)) {			
-//			System.out.println("from: " + spanStart);
-//			System.out.println("until: " + tmpEnd);
+			System.out.println("Fetching Intervall From: " + spanStart + " To: " + tmpEnd);
 
 			Statistics stats = new Statistics(github, repo, spanStart, tmpEnd, GIT_WORKING_DIR);
 			stats.fetchData();
@@ -55,11 +76,9 @@ public class GithubStats {
 			spanStart = DateUtils.addDays(tmpEnd, 1);
 			tmpEnd = DateUtils.addDays(spanStart, INTERVAL_DAYS-1);
 
-			System.out.println("RateLimit: " + github.getRateLimit());
+			System.out.println("RateLimit left: " + github.getRateLimit());
 		}
 		persister.finish();
-				
-		System.out.println("RateLimit: " + github.getRateLimit());
 	}
 
 }
